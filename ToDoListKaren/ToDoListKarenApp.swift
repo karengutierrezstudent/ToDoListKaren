5285dcaf9b9f1fa109a369d898c174e4b5e274ad
Karen Gutierrez <GutierrezKaren1007@gmail.com>
2023-05-18 13:24:11 -0700

    Initial Commit

---
+++ ToDoListKaren/ToDoListKarenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListKarenApp.swift
+//  ToDoListKaren
+//
+//  Created by Karen Gutierrez on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListKarenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
